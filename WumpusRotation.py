be606a63e651294b0ee67c17f07dada00b6b169b
Joshua Miller <codingpupper3033@gmail.com>
2022-12-01 17:13:44 -0500

    Fully working rotation

--- WumpusRotation.py
+++ WumpusRotation.py
@@ -1,51 +1,66 @@
 #!/usr/bin/env python3
+
+import os
 from ev3dev2.motor import *
 from ev3dev2.sensor.lego import *
-
-#Initializing motors
-#motorLeft = LargeMotor('outA'); motorLeft.stop_action = 'hold'
-#motorRight = LargeMotor('outD'); motorRight.stop_action = 'hold'
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
 
 #Constants
-_rotateErrorMargin = 2; # How close to an angle the robot has to be to declare it "good enough"
+#Rotation
+_rotateErrorMargin = 0.5; # How close to an angle the robot has to be to declare it "good enough"
 _rotateSpeedMultiplier = 1; # How touchy the speed of the motor will be. Higher, the more "jerky" the motor will be
+
+#Initializing motors
+motorLeft = LargeMotor('outA'); motorLeft.stop_action = 'hold'
+motorRight = LargeMotor('outB'); motorRight.stop_action = 'hold'
+
+os.system('setfont Lat15-TerminusBold14')
+
+#Initializing sensors
+gyroSensor = GyroSensor(INPUT_3)
+
 
 tempI = 0;
 tempMax = 90;
 rotateAngle = 90;
 
 def rotateTo(angle):
+    print("Rotating to angle: %d"%angle)
     angle = restrictAngle180(angle)
     
     angleLast = restrictAngle180(getGyroAngle())
-    difference = restrictAngle180(angle-angleLast);
+    difference = restrictAngle180(angleLast-angle);
     while (abs(difference) > _rotateErrorMargin):
-        mototPower = math.sin(math.radians(difference/2)) # Restricts Power to be between -1 and 1
-        
+        motorPower = math.sin(math.radians(difference/2)) # Restricts Power to be between -1 and 1
+        motorPower *= _rotateSpeedMultiplier # Speed change by the multiplier
+
+        maxSpeed = min(motorLeft.max_speed,motorRight.max_speed) #Max speed of the slowest motor
+
+        motorSpeed = maxSpeed*motorPower # Speed for the motor to go
+
+        motorLeft.run_forever(speed_sp = -motorSpeed)
+        motorRight.run_forever(speed_sp = motorSpeed)
+
 
         angleLast = restrictAngle180(getGyroAngle())
-        difference = restrictAngle180(angle-angleLast);
+        difference = restrictAngle180(angleLast-angle);
+    motorLeft.stop()
+    motorRight.stop()
+    print("Rotated to angle: %d"%getGyroAngle())
 
 def rotate(angle):
     rotateTo(getGyroAngle()+angle)
 
 def rotateToDirection(direction): # 1 - North, 2 - East, 3 - South, 4 - West
-    rotateTo((direction-1)*90)
+    rotateTo(((direction-1)%4)*90)
 
 def rotateDirection(direction): # 1 - Nothing, 2 - East, 3 - South, 4 - West
-    rotate((direction-1)*90)
+    rotate(((direction-1)%4)*90)
 
 def getGyroAngle():
-    global tempI
-    if tempI == 0:
-        out = tempMax+rotateAngle
-        tempI+=1
-    else:
-        out = (tempMax*math.sin(tempI)/tempI)+rotateAngle
-        tempI+=1
-    return out
+    return -gyroSensor.angle
     
 def restrictAngle180(x):
     return ((x + 180) % 360) - 180
 
-rotateDirection(4)
+gyroSensor.reset() 
