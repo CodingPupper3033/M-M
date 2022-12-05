bfe8ab60c408b273562b3bfea901061e49b55ef4
Joshua Miller <codingpupper3033@gmail.com>
2022-12-05 14:11:14 -0500

    Actually works now

--- the_end_is_uncertain_and_I_have_never_been_so_afraid.py
+++ the_end_is_uncertain_and_I_have_never_been_so_afraid.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 from ev3dev2.sound import Sound
 
 sound = Sound()
 
-song_location = "Songs\TobyMac-LightOfChristmasFtOwlCity.wav"
+song_location = "Songs/MariahCarey-AllIWantforChristmasIsYou.wav"
 
 sound.play_file(wav_file=song_location)
